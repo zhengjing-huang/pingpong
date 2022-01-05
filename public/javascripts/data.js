16f1b5c822a78b14bc86218375167e27cabb168e
Zhengjing Huang <zhengjing.huang@univ-rouen.fr>
2022-01-05 09:27:44 +0100

    ajout de player

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -3,3 +3,26 @@ const GAME_HEIGHT = 500;
 
 const PADDLE_WIDTH = 10;
 const PADDLE_HEIGHT = 100;
+
+
+const leftUser = {
+    x:20,
+    y: (GAME_HEIGHT/2) - (PADDLE_HEIGHT/2),
+    width: PADDLE_WIDTH,
+    height: PADDLE_HEIGHT,
+    color:"white",
+    score:0,
+
+    // update:function(){
+
+    // }
+}
+
+const rightUser = {
+    x:GAME_WIDTH - PADDLE_WIDTH - 20,
+    y:GAME_HEIGHT/2 - PADDLE_HEIGHT/2,
+    width: PADDLE_WIDTH,
+    height: PADDLE_HEIGHT,
+    color:"white",
+    score:0,
+}
