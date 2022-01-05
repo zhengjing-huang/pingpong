fdb5913a28ef5e4dec02cdacad92b00c1778edb4
Zhengjing Huang <zhengjing.huang@univ-rouen.fr>
2022-01-05 10:24:45 +0100

    résolution conflit

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -27,47 +27,27 @@ const rightUser = {
     score:0,
 }
 
+const initialBallX = GAME_WIDTH / 2 + 3;
+const initialBallY = GAME_HEIGHT / 2 + 5;
 
-function move(keyCode){
-    if (keyCode == 90){ //w
-        if (leftUser.y - 5 > 0)
-            leftUser.y -= 9;
-        else
-            leftUser.y = 0
-    }
-    else if (keyCode == 83){ //s
-        if (leftUser.y + 9 < GAME_HEIGHT - leftUser.height)
-            leftUser.y += 9;
-        else 
-            leftUser.y = GAME_HEIGHT - leftUser.height
-    }
-
-    else if (keyCode == 38){ //up
-        if (rightUser.y - 9 > 0)
-            rightUser.y -= 9;
-        else
-            rightUser.y = 0
-    }
-    else if (keyCode == 40){ //down
-        if (rightUser.y + 9 < GAME_HEIGHT - rightUser.height)
-            rightUser.y += 9;
-        else
-            rightUser.y = GAME_HEIGHT - rightUser.height;
-    }
+let initialBall = {
+    x:initialBallX,
+    y:initialBallY,
+    velocityX:2, // vitesse en x
+    velocityY:2, // vitesse en y
+    r:15, //rayon
+    width: 10,
+    color:"white",
+    speed:3.5,
 }
 
-
-const controller = {
-    90:{pressed:false, func:()=>{move(90);}},     //w
-    83:{pressed:false, func:()=>{move(83); }},     //s
-    38:{pressed:false, func:()=>{move(38);}},     //i
-    40:{pressed:false, func:()=>{move(40);}},     //k
-}
-
-function executeMoves(){
-    Object.keys(controller).forEach(key=> {
-        if (controller[key].pressed){
-            controller[key].func()
-        }
-    })
+let ball = {
+    x:initialBallX,
+    y:initialBallY,
+    velocityX:2, // vitesse en x
+    velocityY:2, // vitesse en y
+    r:15, //rayon
+    width: 10,
+    color:"white",
+    speed:3.5,
 }
