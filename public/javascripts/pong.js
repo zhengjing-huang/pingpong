7656939c6c95e0597c6f55c0256ddbc3cd3e6a26
Ryuji-Dono <vasco.couture@univ-rouen.fr>
2022-01-05 10:01:29 +0100

    Ajout fonctions déplacement clavier

--- public/javascripts/pong.js
+++ public/javascripts/pong.js
@@ -17,3 +17,68 @@ drawText(pen,leftUser.score,canvas.width/3,canvas.height/5);
 drawText(pen,rightUser.score,canvas.width*2/3 - 40,canvas.height/5);
 
 drawCircle(pen,ball.x,ball.y, ball.r, ball.color);
+
+
+
+//Player Clavier
+
+
+function setup(){
+
+}
+
+function draw(){
+
+    const arr = [90, 83, 38, 40]
+    arr.forEach(code=>{
+        if (keyIsDown(code)){
+            move(code);
+        }
+    })
+    
+}
+
+function render(){
+    // console.log("render");
+
+    //Réinitialiser le panel canvas
+    drawRect(pen,0,0,canvas.width,canvas.height,"black");
+
+    //record de joueur à gauche
+    drawText(pen,leftUser.score,canvas.width/3,canvas.height/5);
+
+    //record de joueur à droite
+    drawText(pen,rightUser.score,canvas.width*2/3 - 40,canvas.height/5);
+
+    //dessiner les traits
+    drawLines(pen);
+
+    //joueur à gauche
+    drawRect(pen,leftUser.x,leftUser.y,leftUser.width,leftUser.height);
+
+    //joueur à droite
+    drawRect(pen,rightUser.x,rightUser.y,rightUser.width,rightUser.height);
+
+    //balle
+    drawCircle(pen, ball.x, ball.y, ball.r)
+
+    
+}
+
+function play(){
+    if (!GAME_FINISH){
+        update();
+        render();
+    }
+}
+
+function moveAction(e){
+
+    let code = e.code
+    console.log(e.keyCode);
+    move(code)
+}
+
+
+setInterval(play, 10)
+
